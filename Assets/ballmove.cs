02626654d640f977170e868869fd08cc0ef6ff34
Anshika <anshikasinghal411@gmail.com>
2019-06-16 09:58:50 +0530

    mini game

---
+++ Assets/ballmove.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ballmove : MonoBehaviour {
+
+	public float force;
+	public Transform ballchecker;
+
+	private int count;
+	void Start () {
+		//GetComponent<Rigidbody2D>().AddForce (new Vector2 (1f, 0.5f) * Time.deltaTime * force);
+	// ball going up with force...now up only wen we press space
+		//wenever level loadede start
+
+
+		transform.position=new Vector2(0.0f,-2.19f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Space)) {
+			GetComponent<Rigidbody2D> ().AddForce (new Vector2 (1f, 0.5f) * Time.deltaTime * force);
+		}
+		if (transform.position.y < ballchecker.transform.position.y) {
+			Application.LoadLevel (Application.loadedLevel);
+		}
+	}
+		/*void OnTriggerEnter2D(Collider other)
+		{
+			if (other.gameObject.Tag ("pickup")) 
+			{
+				
+				count = count + 1;
+			OnGUI ();
+			}
+
+		}
+		
+	void OnGUI()
+	{
+		GUILayout.Label (count.ToString ());
+	
+	}*/
+}
+
---
+++ Assets/move.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+	Vector2 position;
+	public float speed;
+	void Start () {
+		
+		position = transform.position; //to store original pos of paddle..den again put changed pos in tranform.pos
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		float moveX = Input.GetAxis ("Horizontal"); //get input from keys and store in movex
+		position.x +=moveX * Time.deltaTime * speed;
+		position.x = Mathf.Clamp (position.x, -10.0f, 10.0f);
+		transform.position = position;
+
+	
+	}
+}
